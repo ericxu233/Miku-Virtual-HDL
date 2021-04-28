88a7e9c406b98b76907dee662ea6312f166b8603
eric <xuhanyang5@gmail.com>
2021-04-27 22:21:31 -0400

    added the base class for logic gates

---
+++ logic_gate.py
@@ -0,0 +1,16 @@
+
+#This is the file that contains the core objects for the representation of logic gates
+
+class logicGate:
+    def __init__(self, typev, name):
+        self.gateType = typev
+        self.gateName = name
+        self.output = -1
+        self.next_gate = None
+        self.input = []
+
+
+    def add_next(next):
+        next_gate = next
+    
+    
