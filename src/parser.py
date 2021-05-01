071e1bd3cbd307a88c7a63f90653e3c3981e69a2
eric <xuhanyang5@gmail.com>
2021-04-30 23:27:43 -0400

    added module for parser engine

---
+++ src/parser.py
@@ -0,0 +1,14 @@
+from logic_gate import logicGate
+from logic_gate import orGate
+from logic_gate import andGate
+from logic_gate import notGate
+from logic_gate import norGate
+from logic_gate import nandGate
+from logic_gate import xorGate
+from logic_gate import xnorGate
+
+outputs = []
+
+def parser():
+    a = 0
+
