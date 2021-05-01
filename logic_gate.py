07f859552d3916683ec865f3af4926364da460d3
eric <xuhanyang5@gmail.com>
2021-04-30 22:23:17 -0400

    added and or not gate classed

--- logic_gate.py
+++ logic_gate.py
@@ -1,6 +1,7 @@
+all_gates = []
+
 
 #This is the file that contains the core objects for the representation of logic gates
-
 class logicGate:
     def __init__(self, typev, name):
         self.gateType = typev
@@ -9,8 +10,37 @@ class logicGate:
         self.next_gate = " "
         self.input = []
 
+        all_gates[name] = self
+    
+    def add_input(wire):
+        input.insert(len(input), wire)
 
-    def add_next(next):
-        next_gate = next
+
+
+class orGate(logicGate):
+    def __init__(self, typev, name):
+        logicGate.__init__(self, typev, name)
+
+    def compute_result():
+        output = False
+        for x in input:
+            output = output or x
+
+
     
-    
+class andGate(logicGate):
+    def __init__(self, typev, name):
+        logicGate.__init__(self, typev, name)
+
+    def compute_result():
+        output = True
+        for x in input:
+            output = output and x
+
+
+class notGate(logicGate):
+    def __init__(self, typev, name):
+        logicGate.__init__(self, typev, name)
+
+    def compute_result():
+        output = not input[0]
