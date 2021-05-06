c4cee3d097bc0e15af3f1c1e8bdc47a2598f9427
eric <xuhanyang5@gmail.com>
2021-05-06 00:00:34 -0400

    partially finished logic gate creation function

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,5 @@
+import sys
+
 from logic_gate import logicGate
 from logic_gate import orGate
 from logic_gate import andGate
@@ -12,24 +14,58 @@ outputs = []
 
 
 def create_gate(command, identifier):
-    if command == "morr":
-        all_gates[identifier] = orGate(command[1:], ideentifier)
-    elif command == "mand":
-        all_gates[identifier] = andGate(command[1:], ideentifier)
-    elif command == "mnot":
-        all_gates[identifier] = notGate(command[1:], ideentifier)
+    result = True
 
+    if command == "morr":
+        all_gates[identifier] = orGate(command[1:], identifier)
+    elif command == "mand":
+        all_gates[identifier] = andGate(command[1:], identifier)
+    elif command == "mnot":
+        all_gates[identifier] = notGate(command[1:], identifier)
+    elif command == "mnor":
+        all_gates[identifier] = norGate(command[1:], identifier)
+    elif command == "mnad":
+        all_gates[identifier] = nandGate(command[1:], identifier)
+    elif command == "mxor":
+        all_gates[identifier] = xorGate(command[1:], identifier)
+    elif command == "mxnr":
+        all_gates[identifier] = xnorGate(command[1:], identifier)
+    elif command == "output":
+        outputs.insert(len(outputs), identifier)
+    else:
+        result = False
+
+    return result
+
+def create_output(command, identifier):
+    result = True
+
+    if command == "output":
+        outputs.insert(len(outputs), identifier)
+    else:
+        result = False
+    
+    return result
 
 
 def parser():
     file = open("../user_scipt/circuit.txt", "r")
     lines = file.readlines()
+    counter = 0
 
     for x in lines:
         x.replace("(", " ")
         x.replace(",", " ")
         x.replace(")", " ")
-        components.split()
+        components = x.split()
+        insertion_operation = create_gate(components[0], components[1])
+
+        #current problem: how to parse different types of commands eligently
+        if insertion_operation:
+            #...
+        
+
+
         #not done yet...
 
             
